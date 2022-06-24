bf584ce89b5e44594a22b440788c8e4c62d79f5d
Andres D'Amelio <andresdamelio24@gmail.com>
2022-06-24 18:49:05 -0400

    add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,12 @@ import '../styles/icons.css';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={false} defaultTheme="dark" attribute="class">
+      <NextNProgress color="#fabf2b" height={3} showOnShallow={true} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
