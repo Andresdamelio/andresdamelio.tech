8dea96a0559dc039c57a4af3b7b02b33920e072d
Andres D'Amelio <andresdamelio24@gmail.com>
2022-06-25 15:55:55 -0400

    ISR on demand with articles and project

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,5 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { getStaticPaths as getPostPaths } from 'pages/blog/[slug]';
+import { getStaticPaths as getProjectPaths } from 'pages/portafolio/[slug]';
+import { ObjectAny } from 'interfaces';
+
 type ApiResponse = NextApiResponse & {
   unstable_revalidate: (path: string) => void;
 };
@@ -10,11 +14,25 @@ export default async (req: NextApiRequest, res: ApiResponse) => {
   }
 
   try {
-    await res.unstable_revalidate('/sobre-mi');
-    await res.unstable_revalidate('/resumen');
-    await res.unstable_revalidate('/portafolio');
-    await res.unstable_revalidate('/blog');
-    await res.unstable_revalidate('/contacto');
+    const staticPostPaths = await getPostPaths({});
+    const staticProjectPaths = await getProjectPaths({});
+
+    const revalidatePostPaths = (staticPostPaths.paths as ObjectAny[]).map(
+      ({ params: { slug } }) => res.unstable_revalidate(`/blog/${slug}`)
+    );
+    const revalidateProjectPaths = (
+      staticProjectPaths.paths as ObjectAny[]
+    ).map(({ params: { slug } }) => res.unstable_revalidate(`/blog/${slug}`));
+
+    await Promise.all([
+      res.unstable_revalidate('/sobre-mi'),
+      res.unstable_revalidate('/resumen'),
+      res.unstable_revalidate('/portafolio'),
+      res.unstable_revalidate('/blog'),
+      res.unstable_revalidate('/contacto'),
+      ...revalidatePostPaths,
+      ...revalidateProjectPaths,
+    ]);
 
     return res.status(200).json({ revalidated: true });
   } catch (err) {
