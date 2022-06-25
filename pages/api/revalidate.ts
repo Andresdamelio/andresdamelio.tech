1eeb1f5cfde4f77b10a6d41c226611219a367f14
Andres D'Amelio <andresdamelio24@gmail.com>
2022-06-25 13:58:03 -0400

    revalidate post blog

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,48 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { getStaticPaths as getPostPaths } from 'pages/blog/[slug]';
+import { ObjectAny } from 'interfaces';
+
 type ApiResponse = NextApiResponse & {
   unstable_revalidate: (path: string) => void;
 };
 
 export default async (req: NextApiRequest, res: ApiResponse) => {
-  const body = await getRawBody(req);
-  // if (!body) {
-  //   res.status(400).send('Bad request (no body)');
-  //   return;
-  // }
-
-  const jsonBody = JSON.parse(body as string);
-  // eslint-disable-next-line no-console
-  console.log('body', jsonBody);
   if (req.query.secret !== process.env.TOKEN_REVALIDATE) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
   try {
-    const slug = jsonBody.post?.slug;
+    const staticPaths = await getPostPaths({});
 
-    if (slug) {
-      await res.unstable_revalidate(`/blog/${slug}`);
-    }
-    await res.unstable_revalidate('/sobre-mi');
-    await res.unstable_revalidate('/resumen');
-    await res.unstable_revalidate('/portafolio');
-    await res.unstable_revalidate('/blog');
-    await res.unstable_revalidate('/contacto');
+    const revalidatePostPaths = staticPaths.map(({ params }: ObjectAny) =>
+      res.unstable_revalidate(`/blog/${params.slug}`)
+    );
+
+    await Promise.all([
+      res.unstable_revalidate('/sobre-mi'),
+      res.unstable_revalidate('/resumen'),
+      res.unstable_revalidate('/portafolio'),
+      res.unstable_revalidate('/blog'),
+      res.unstable_revalidate('/contacto'),
+      ...revalidatePostPaths,
+    ]);
 
     return res.status(200).json({ revalidated: true });
   } catch (err) {
     return res.status(500).json({ message: 'Error validating' });
   }
 };
-
-function getRawBody(req: NextApiRequest) {
-  return new Promise((resolve, _) => {
-    const bodyChunks: Uint8Array[] = [];
-    req.on('end', () => {
-      const rawBody = Buffer.concat(bodyChunks).toString('utf8');
-      resolve(rawBody);
-    });
-    req.on('data', (chunk) => bodyChunks.push(chunk));
-  });
-}
