d2811f1c97a20fe31108645eef13e9e282a1f1b7
Andres D'Amelio <andresdamelio24@gmail.com>
2022-06-25 13:29:59 -0400

    test on demand

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -5,11 +5,25 @@ type ApiResponse = NextApiResponse & {
 };
 
 export default async (req: NextApiRequest, res: ApiResponse) => {
+  const body = await getRawBody(req);
+  // if (!body) {
+  //   res.status(400).send('Bad request (no body)');
+  //   return;
+  // }
+
+  const jsonBody = JSON.parse(body as string);
+  // eslint-disable-next-line no-console
+  console.log('body', jsonBody);
   if (req.query.secret !== process.env.TOKEN_REVALIDATE) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
   try {
+    const slug = jsonBody.post?.slug;
+
+    if (slug) {
+      await res.unstable_revalidate(`/blog/${slug}`);
+    }
     await res.unstable_revalidate('/sobre-mi');
     await res.unstable_revalidate('/resumen');
     await res.unstable_revalidate('/portafolio');
@@ -21,3 +35,14 @@ export default async (req: NextApiRequest, res: ApiResponse) => {
     return res.status(500).json({ message: 'Error validating' });
   }
 };
+
+function getRawBody(req: NextApiRequest) {
+  return new Promise((resolve, _) => {
+    const bodyChunks: Uint8Array[] = [];
+    req.on('end', () => {
+      const rawBody = Buffer.concat(bodyChunks).toString('utf8');
+      resolve(rawBody);
+    });
+    req.on('data', (chunk) => bodyChunks.push(chunk));
+  });
+}
